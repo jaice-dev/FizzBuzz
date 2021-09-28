3dc938353a977bd8a4938441c980f1f69b7e7d05
Jordan <jordan.d.carlton@gmail.com>
2021-09-28 09:29:02 +0100

    added FizzBuzz

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                if (i % 3 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
                 }
                 else
                 {
