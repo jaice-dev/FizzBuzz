83af2890622d4b1aa5632a172ea24b69b52cb7e9
Jordan <jordan.d.carlton@gmail.com>
2021-09-28 09:20:18 +0100

    counts to 100

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
